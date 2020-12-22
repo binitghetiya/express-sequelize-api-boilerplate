bd75e897a406fd2000907d5a0a5a1cbf589d3610
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2020-12-22 12:56:36 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,107 @@
 require('@babel/register');
 /* eslint-disable no-console */
+const chalk = require('chalk');
+const dotenv = require('dotenv');
 const cluster = require('cluster');
-const os = require('os');
+const numCores = require('os').cpus().length;
 const app = require('./app');
 
-if (cluster.isMaster && process.env.NODE_ENV === 'production') {
-  const numWorkers = os.cpus().length;
+// Handle uncaught exceptions
+process.on('uncaughtException', (uncaughtExc) => {
+  // Won't execute
+  console.log(chalk.bgRed('UNCAUGHT EXCEPTION! 💥 Shutting down...'));
+  console.log('uncaughtException Err::', uncaughtExc);
+  console.log('uncaughtException Stack::', JSON.stringify(uncaughtExc.stack));
+  process.exit(1);
+});
 
-  console.log(`Master cluster setting up ${numWorkers} workers...`);
+// Setup number of worker processes to share port which will be defined while setting up server
+const workers = [];
+const setupWorkerProcesses = () => {
+  // Read number of cores on system
+  console.log(`Master cluster setting up ${numCores} workers`);
 
-  for (let i = 0; i < numWorkers; i += 1) {
-    cluster.fork();
+  // Iterate on number of cores need to be utilized by an application
+  // Current example will utilize all of them
+  for (let i = 0; i < numCores; i++) {
+    // Creating workers and pushing reference in an array
+    // these references can be used to receive messages from workers
+    workers.push(cluster.fork());
+
+    // Receive messages from worker process
+    workers[i].on('message', function (message) {
+      console.log(message);
+    });
   }
 
-  cluster.on('online', (worker) => {
-    console.log(`Worker ${worker.process.pid} is online`);
+  // Process is clustered on a core and process id is assigned
+  cluster.on('online', function (worker) {
+    console.log(`Worker ${worker.process.pid} is listening`);
   });
 
-  cluster.on('exit', (worker, code, signal) => {
+  // If any of the worker process dies then start a new one by simply forking another one
+  cluster.on('exit', function (worker, code, signal) {
     console.log(
-      `Worker ${
-        worker.process.pid
-      } died with code: ${code}, and signal: ${signal}`,
+      `Worker ${worker.process.pid} died with code: ${code}, and signal: ${signal}`
     );
     console.log('Starting a new worker');
     cluster.fork();
+    workers.push(cluster.fork());
+    // Receive messages from worker process
+    workers[workers.length - 1].on('message', function (message) {
+      console.log(message);
+    });
   });
+};
+
+// Setup an express server and define port to listen all incoming requests for this application
+const setUpExpress = () => {
+  dotenv.config({ path: '.env' });
+  
+  const port = process.env.APP_PORT || 3000;
+
+  const server = app.listen(port, () => {
+    console.log(`App running on port ${chalk.greenBright(port)}...`);
+  });
+
+  // In case of an error
+  app.on('error', (appErr, appCtx) => {
+    console.error('app error', appErr.stack);
+    console.error('on url', appCtx.req.url);
+    console.error('with headers', appCtx.req.headers);
+  });
+
+  // Handle unhandled promise rejections
+  process.on('unhandledRejection', (err) => {
+    console.log(chalk.bgRed('UNHANDLED REJECTION! 💥 Shutting down...'));
+    console.log(err.name, err.message);
+    // Close server & exit process
+    server.close(() => {
+      process.exit(1);
+    });
+  });
+
+  process.on('SIGTERM', () => {
+    console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
+    server.close(() => {
+      console.log('💥 Process terminated!');
+    });
+  });
+};
+
+// Setup server either with clustering or without it
+const setupServer = (isClusterRequired) => {
+  // If it is a master process then call setting up worker process
+  if (isClusterRequired && cluster.isMaster) {
+    setupWorkerProcesses();
+  } else {
+    // Setup server configurations and share port address for incoming requests
+    setUpExpress();
+  }
+};
+
+if (process.env.NODE_ENV === 'production') {
+  setupServer(true);
 } else {
-  app.listen(process.env.APP_PORT || 3000, () => {
-    console.log(`Process ${process.pid} is listening to all incoming requests`);
-  });
+  setupServer(false);
 }
